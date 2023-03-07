30c409157ed4f431570303726633813b5b332f32
Arwin Azizi <arwin2002@hotmail.se>
2023-03-07 12:07:39 +0100

    change

--- rock.js
+++ rock.js
@@ -26,11 +26,11 @@ function getPlayerChoice() {
         getBothChoices('p')
     })
 
-<<<<<<< HEAD
     scissors.addEventListener('click', function() {
         getBothChoices('s')
     })
 };
+
 getPlayerChoice()
 
 function win() {
@@ -45,19 +45,6 @@ function lose() {
 }
 function tie() {
     result.innerHTML = 'This round was a tie mr bossman'
-=======
-    if (playerChoice === computerChoice) {
-        return "tie";
-     } else if (
-        (playerChoice === "rock" && computerChoice === "scissors") ||
-        (playerChoice === "Scissors" && computerChoice === "paper") ||
-        (playerChoice === "paper" && computerChoise === "rock")
-     ) {
-        return "player";
-     } else {
-        return "computer";
-     }
->>>>>>> c055617356525b604cfac0498f28827016abdac0
 }
 
 
@@ -94,50 +81,6 @@ function getBothChoices(meChoice) {
             break;
     }
 };
-<<<<<<< HEAD
-getBothChoices()
-=======
 
-function getPlayerChoice() {
-    let validatedInput = false;
-    while(validatedInput == false) {
-        const choice = prompt("Rock Paper Scissors");
-        if (choice == null) {
-            continue;
-        } 
-        const choiceInLower = choice.toLowerCase();
-        if (choices.includes(choiceInLower))
-        validatedInput = true;
-        return choiceInLower;
-    }
-}
- 
+getBothChoices();
 
-function game() {
-    let scorePlayer = 0;
-    let scoreComputer = 0;
-    console.log("Welcome!")
-
-    for (let i = 0; i < 5; i++) {
-        const playerChoice = getPlayerChoice();
-        const computerChoice = getComputerChoice();
-        console.log(playRound(playerChoice, computerChoice))
-        console.log("---------")
-        if (checkWinner(playerChoice, computerChoice) == "player") {
-            scorePlayer++
-        }
-        else if (checkWinner(playerChoice, computerChoice) == "computer") {
-            scoreComputer++
-        }
-    }
-    console.log("Game over! ")
-    if (scorePlayer > scoreComputer) {
-        console.log("You was the winner!") 
-    } else if (scoreComputer < scorePlayer) {
-        console.log("You lose! Computer was the winner.")
-    } else {
-        console.log("We have a tie!")
-    }
-}
-
->>>>>>> c055617356525b604cfac0498f28827016abdac0
