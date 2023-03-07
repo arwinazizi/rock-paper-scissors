ba2bf5c8d6216b8f1f8d9ef862193e5e27a9d132
Arwin Azizi <arwin2002@hotmail.se>
2023-03-07 08:31:18 +0100

    added ui to my rock paper scissors project

--- rock.js
+++ rock.js
@@ -1,79 +1,83 @@
-console.log("Hi, lets have somee fun!")
 
-const choices = ["rock", "paper", "scissors"];
+let pcScore = 0;
+let meScore = 0;
+const computerScore = document.querySelector('#computerscore')
+const arwinScore = document.querySelector('#arwinscore')
+const rock = document.querySelector('.rock')
+const paper = document.querySelector('.paper')
+const scissors = document.querySelector('.scissors')
+const result = document.querySelector('.result')
+const reset = document.querySelector('.reset')
+
 
 function getComputerChoice() {
+    const choices = ['r', 'p', 's'];
     const randomIndex = Math.floor(Math.random() * choices.length);
     return choices[randomIndex]
 }
 
+function getPlayerChoice() {
+    
+    rock.addEventListener('click', function() {
+        getBothChoices('r')
+    })
 
-function checkWinner(playerChoice, computerChoice) {
-    playerChoice = playerChoice.toLowerCase();
+    paper.addEventListener('click', function() {
+        getBothChoices('p')
+    })
 
-    if (playerChoice === computerChoice) {
-        return "tie";
-     } else if (
-        (playerChoice === "rock" && computerChoice === "scissors") ||
-        (playerChoice === "Scissors" && computerChoice === "paper") ||
-        (playerChoice === "paper" && computerChoice === "rock")
-     ) {
-        return "player";
-     } else {
-        return "computer";
-     }
+    scissors.addEventListener('click', function() {
+        getBothChoices('s')
+    })
+};
+getPlayerChoice()
+
+function win() {
+    meScore++
+    arwinScore.innerHTML = meScore;
+    result.innerHTML = 'You won this round boss!'
+}
+function lose() {
+    pcScore++
+    computerScore.innerHTML = pcScore;
+    result.innerHTML =  'You loose this round, Loser!'
+}
+function tie() {
+    result.innerHTML = 'This round was a tie mr bossman'
 }
 
-function playRound(playerChoice, computerChoice) {
-    const result = checkWinner(playerChoice, computerChoice);
-    if (result == "tie") {
-        return "Its a tie" 
-    } else if (result == "player") {
-        return `You win! ${playerChoice} beats ${computerChoice}`
-    } else {
-        return `Loser! ${computerChoice} beats ${playerChoice}`
+
+function restart() {
+    reset.addEventListener('click', function() {
+        meScore = 0;
+        pcScore = 0;
+        arwinScore.innerHTML = meScore;
+        computerScore.innerHTML = pcScore;
+    })
+}
+restart()
+
+
+function getBothChoices(meChoice) {
+
+    const pcChoice = getComputerChoice();
+
+    switch(meChoice + pcChoice) {
+        case 'rs':
+        case 'pr':
+        case 'sp':
+            win()
+            break;
+        case 'sr':
+        case 'rp':
+        case 'ps':
+            lose()
+            break;
+        case 'rr':
+        case 'ss':
+        case 'pp':
+            tie()
+            break;
     }
 };
-
-function getPlayerChoice() {
-    let validatedInput = false;
-    while(validatedInput == false) {
-        const choice = prompt("Rock Paper Scissors");
-        if (choice == null) {
-            continue;
-        } 
-        const choiceInLower = choice.toLowerCase();
-        if (choices.includes(choiceInLower))
-        validatedInput = true;
-        return choiceInLower;
-    }
-}
- 
-
-function game() {
-    let scorePlayer = 0;
-    let scoreComputer = 0;
-    console.log("Welcome!")
-
-    for (let i = 0; i < 5; i++) {
-        const playerChoice = getPlayerChoice();
-        const computerChoice = getComputerChoice();
-        console.log(playRound(playerChoice, computerChoice))
-        console.log("---------")
-        if (checkWinner(playerChoice, computerChoice) == "player") {
-            scorePlayer++
-        }
-        else if (checkWinner(playerChoice, computerChoice) == "computer") {
-            scoreComputer++
-        }
-    }
-    console.log("Game over! ")
-    if (scorePlayer > scoreComputer) {
-        console.log("You was the winner!") 
-    } else if (scoreComputer > scorePlayer) {
-        console.log("You lose! Computer was the winner.")
-    } else {
-        console.log("We have a tie!")
-    }
-}
-
+getBothChoices()
